b7c31382295c204b4dafc98007642633ecaa12b9
jousef1 <yousefel560@gmail.com>
2023-02-05 22:37:53 +0800

    first commit

---
+++ hoo_bank_app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles'
+
+const App = () =>  (
+    <div className='bg-secondary w-full overflow-hidden'>
+      <div className=''>
+
+      </div>
+    </div>
+  );
+
+
+export default App
